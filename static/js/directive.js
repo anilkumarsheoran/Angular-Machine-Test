f5c37204cc7b6495ce58e8e282984d23b32d8cfd
anilkumarsheoran <anilkumar.sheoran@gmail.com>
2016-07-27 00:03:39 +0530

    dir

--- static/js/directive.js
+++ static/js/directive.js
@@ -1,26 +1,21 @@
-app.directive("myValidateForm", function () {
-    // requires an isloated model
+app.directive('validationFormNumber', function () {
     return {
-        // restrict to an attribute type.
-        restrict: 'A',
-        // element must have ng-model attribute.
         require: 'ngModel',
-        link: function (scope, ele, attrs, ctrl) {
+        link: function (scope, elem, attrs, ctrl) {
+            ctrl.$parsers.unshift(checkForno);
+            ctrl.$formatters.unshift(checkForno);
 
-            // add a parser that will process each time the value is
-            // parsed into the model when the user updates it.
-            ctrl.$parsers.unshift(function (value) {
-                if (value) {
-                    // test and set the validity after update.
-                 //   var valid = value.charAt(0) == 'A' || value.charAt(0) == 'a';
-                    ctrl.$setValidity('invalidAiportCode', 'True');
+            function checkForno(viewValue) {
+                if (parseInt(viewValue)%2 === 0) {
+                    ctrl.$setValidity('number', true);
                 }
-
-                // if it's valid, return the value to the model,
-                // otherwise return undefined.
-                return valid ? value : undefined;
-            });
-
+                else {
+                    ctrl.$setValidity('number', false);
+                }
+                return viewValue;
+            }
         }
-    }
+    };
+
 });
+
